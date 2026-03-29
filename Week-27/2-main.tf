1c89f3d29a5f2ecfeb4ffaa37bdc997c03ff69eb
JGordonHall <jarred.p.hall@gmail.com>
2026-03-28 21:46:21 -0700

    10th-Push

--- Week-27/2-main.tf
+++ Week-27/2-main.tf
@@ -21,10 +21,10 @@ resource "aws_s3_object" "object-jpg" {
 }
 
 resource "aws_s3_object" "Jenkins_Proof" {
-  for_each = fileset("${path.module}/Jenkins_proof", "**")
+  for_each = fileset("${path.module}/Jenkins_Proof", "**")
 
   bucket       = aws_s3_bucket.frontend.id
   key          = "Jenkins_Proof/${each.value}"
-  source       = "${path.module}/Jenkins_proof/${each.value}"
+  source       = "${path.module}/Jenkins_Proof/${each.value}"
   content_type = "image/png"
 }
