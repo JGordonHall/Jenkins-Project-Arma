2273dcf94f717a15ed96cec901d2e4c047759ce3
JGordonHall <jarred.p.hall@gmail.com>
2026-03-30 16:29:17 -0700

    Last-est Push

--- Week-27/2-main.tf
+++ Week-27/2-main.tf
@@ -8,6 +8,34 @@ resource "aws_s3_bucket" "frontend" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "pab_name" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+}
+
+resource "aws_s3_bucket_policy" "bp_name" {
+  bucket = aws_s3_bucket.frontend.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.pab_name]
+}
+
 resource "aws_s3_object" "object-txt" {
   bucket = aws_s3_bucket.frontend.id
   key    = "Armageddon-Proof/armageddon-link.txt"
